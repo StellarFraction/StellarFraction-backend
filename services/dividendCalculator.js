447f1ca239a2b51f3bd82c6b97b1484672bae12a
JTKaduma <jasonbahago@gmail.com>
2026-07-14 20:31:43 +0100

    feat: calculate payouts using six-decimal USDC units

--- services/dividendCalculator.js
+++ services/dividendCalculator.js
@@ -1,5 +1,8 @@
+const USDC_SCALE = 1_000_000;
+
 const calculateDividendPayouts = (stakers, amountUSDC) => {
   const totalShares = stakers.reduce((total, staker) => total + staker.shares, 0);
+  const amountInMicroUSDC = Math.round(amountUSDC * USDC_SCALE);
 
   return {
     amountUSDC,
@@ -7,7 +10,7 @@ const calculateDividendPayouts = (stakers, amountUSDC) => {
       stakerId: staker.id,
       address: staker.address,
       shares: staker.shares,
-      amountUSDC: amountUSDC * staker.shares / totalShares
+      amountUSDC: Math.floor(amountInMicroUSDC * staker.shares / totalShares) / USDC_SCALE
     }))
   };
 };
