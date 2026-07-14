97f5c29e5e1e3b95245f7a18b6131610c8eb5115
JTKaduma <jasonbahago@gmail.com>
2026-07-14 20:33:26 +0100

    fix: assign rounding remainder without overpayment

--- services/dividendCalculator.js
+++ services/dividendCalculator.js
@@ -3,14 +3,22 @@ const USDC_SCALE = 1_000_000;
 const calculateDividendPayouts = (stakers, amountUSDC) => {
   const totalShares = stakers.reduce((total, staker) => total + staker.shares, 0);
   const amountInMicroUSDC = Math.round(amountUSDC * USDC_SCALE);
+  const allocations = stakers.map(staker =>
+    Math.floor(amountInMicroUSDC * staker.shares / totalShares)
+  );
+  const allocated = allocations.reduce((total, allocation) => total + allocation, 0);
+
+  if (allocations.length > 0) {
+    allocations[allocations.length - 1] += amountInMicroUSDC - allocated;
+  }
 
   return {
     amountUSDC,
-    payouts: stakers.map(staker => ({
+    payouts: stakers.map((staker, index) => ({
       stakerId: staker.id,
       address: staker.address,
       shares: staker.shares,
-      amountUSDC: Math.floor(amountInMicroUSDC * staker.shares / totalShares) / USDC_SCALE
+      amountUSDC: allocations[index] / USDC_SCALE
     }))
   };
 };
